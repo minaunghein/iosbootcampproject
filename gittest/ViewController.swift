d689d0c465c6aefc3c7d928f08c31a90d4331cbb
Min Aung Hein <minaunghein@gmail.com>
2020-12-30 18:32:54 +0630

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Min Aung Hein on 12/30/20.
+//  Copyright © 2020 Min Aung Hein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
