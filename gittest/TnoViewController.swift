e40dd39ed00f6c34b05b269adfc363dbd7c88df5
TheinDev <theinnaingdev@gmail.com>
2021-01-05 18:40:21 +0630

    ViewTno storyboard is added.

---
+++ gittest/TnoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TnoViewController.swift
+//  gittest
+//
+//  Created by Etrade Developer on 05/01/2021.
+//  Copyright © 2021 Min Aung Hein. All rights reserved.
+//
+
+import UIKit
+
+class TnoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
